344d02f2611a8a8fc1d77043670a63a3b5c83e96
Luis Usero Reyes <l.usero.dam@gmail.com>
2015-04-08 14:48:39 +0200

    ejercicio del tema

--- src/Ejercicios/Ejercicio4.java
+++ src/Ejercicios/Ejercicio4.java
@@ -1,28 +1,35 @@
 package Ejercicios;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Libro> lista = new ArrayList<Libro>();
+		Set<Libro> listaDiezLibros = new HashSet<Libro>();
 		//creamos el descriptor del fiochero
 		File inFile = new File("/home/matinal/libros.csv");
 		//creamos el flujo de entrada
 		try (BufferedReader in = new BufferedReader(new FileReader(inFile));){
 			String linea=in.readLine();//leo primera linea y la desecho
-			Libro libro; //creo la referencia
+			Libro libros; //creo la referencia
 			while((linea=in.readLine())!=null){
-				String[] campo = linea.split(";");
-				libro = new Libro(campo[0],campo[3], campo[6]);
-				lista.add(libro);
+				String[] campo = linea.split(":");
+				//libros = new Libro(campo[0],campo[3],campo[6]);
+				//lista.add(libros);
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -31,10 +38,27 @@ public class Ejercicio4 {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-	}
+		//debemos sacar diez libros al azar y añadirlos a la nueva lista
+		Libro libro;
+		do{
+			//posicion aleatoria
+			int posicionLibroLista = (int) (Math.random()*lista.size());
+			//obtenemos libros de la lista
+			libro = lista.get(posicionLibroLista);
+			//lo añadimos a la nueva lista
+			listaDiezLibros.add(libro);
+		}while(listaDiezLibros.size() <10);
+		File outFile = new File("/home/matinal/libros.dat");
+		try (ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(outFile)))){
 
+			out.writeObject(listaDiezLibros);
+		
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 }
-class Libro{
+class Libro implements Serializable{
 	private String titulo;
 	private String autor;
 	private String isbn;
@@ -54,5 +78,5 @@ class Libro{
 				+ isbn + "]";
 	}
 	
-	
+	}	
 }
---
+++ src/Ejercicios/Ejercicio6.java
@@ -0,0 +1,44 @@
+package Ejercicios;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//descriptor del fichero de texto
+		String path = "/home/matinal/";
+		File inFile = new File(path+"Cadenas.java");
+		//dos ficheros de salida
+		File outFile1 = new File(path+"Cadena1.java");
+		File outFile2 = new File(path+"Cadena2.java");
+		//creamos un array de 1024 byte
+		char[] array1 = new char[1024];
+		//tamaño del 2 array es el tamaño del fichero menos el tamaño del 1 array
+		char[] array2 = new char[(int) (inFile.length()-array1.length)];
+		//stream de lectura
+		try (BufferedReader in = new BufferedReader(new FileReader(inFile));
+				BufferedWriter out1 = new BufferedWriter(new FileWriter(outFile1))
+				){
+			//añadir los 1024 byte al array
+			in.read(array1); out1.flush();
+			out1.write(array1);
+			//el resto lo añadimos al segundo array
+			//in.read(array2, array1.length, array2.length);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+}
